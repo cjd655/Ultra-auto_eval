e9ff4ed4c22faa935d2b1eda2afe5ecc138f6278
cjd655 <2792131611@qq.com>
2025-10-21 21:31:28 +0800

    Update 1_main.py

--- 1_main.py
+++ 1_main.py
@@ -1,4 +1,20 @@
-#
-#
-#
-print('Hello World')
+\\202511716132
+\\2792131611@qq.com
+\\陈俊东
+int main() {
+    int s;
+    scanf ("%d",&s);
+    if (s<0 || s>=100){
+        printf("成绩错误\n");
+}else if (s>=90 && s<=100) {
+    printf("A\n");
+}else if (s >= 80 && s<=89){
+    printf("B\n");
+}else if (s >= 70 && s<=79){
+}else if (s >= 60 && s<=69){
+    printf("D\n");
+}else {
+    printf("E\n");
+}
+    return 0;
+}
