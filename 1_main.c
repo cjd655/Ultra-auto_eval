e50b7db30d85bdd50a724644dae693eca2966a17
cjd655 <2792131611@qq.com>
2025-10-21 20:58:44 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,15 @@
-//
-//
-//
+//202511716132
+//2792131611@qq.com
+//陈俊东
 #include <stdio.h>
-int main()
+int main() 
 {
-    printf("Hello World");
+    float celsius, fahrenheit;
+    //获取用户输入的摄氏度
+    scanf ("%f", &celsius);
+    //进行温度转换
+    fahrenheit =celsius * 9.0/5.0+32;
+    //输出华氏温度，保留一位小数
+    printf ("%.1f\n",fahrenheit);
     return 0;
 }
