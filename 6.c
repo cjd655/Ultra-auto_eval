bd0de4938af3a5526560b7ab0ba5e9b2f94f95e3
陈俊东 <2792131611@qq.com>
2025-11-01 18:45:34 +0800

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,17 @@
+//202511716132
+//2792131611@qq.com
+//MY_URL
+#include <stdio.h>
+int main() {
+    int arr[5], i,num;{
+        do{scanf("%d",&num);
+        }while (num % 2 !=0);
+        arr[i] = num;
+    }
+    for(i = 0;i < 5;i++){
+        printf("%d",arr[i]);
+        if(i < 4)printf(" ");
+    }
+    printf("\n");
+    return 0;
+     }
